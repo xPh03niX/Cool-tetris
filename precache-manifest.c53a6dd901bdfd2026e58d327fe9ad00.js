06b5e52f1cd98b152549087f959654bb191d7d82
xPh03niX <nothidding001@gmail.com>
2020-05-19 02:07:43 -0400

    Updates

---
+++ precache-manifest.c53a6dd901bdfd2026e58d327fe9ad00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d937e8d0674ea822104bb31622fe50",
+    "url": "/Cool-tetris/index.html"
+  },
+  {
+    "revision": "970c5c534cf8d046908e",
+    "url": "/Cool-tetris/static/css/main.e75c5f2b.chunk.css"
+  },
+  {
+    "revision": "400603519bc95952bc44",
+    "url": "/Cool-tetris/static/js/2.a5da235e.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Cool-tetris/static/js/2.a5da235e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970c5c534cf8d046908e",
+    "url": "/Cool-tetris/static/js/main.2029e277.chunk.js"
+  },
+  {
+    "revision": "cd9e9beee3390d3ae608",
+    "url": "/Cool-tetris/static/js/runtime-main.6ad665f8.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/Cool-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/Cool-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
